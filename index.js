5f4b9275b2c1ede1c7dbeab8eeed4daf2c78404d
Damon sarceno <Damonsarceno@gmail.com>
2023-05-12 12:10:29 -0400

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+function writeCards(cards, events) {
+  let newArray=[]
+  for (let i = 0; i < cards.length; i++) {
+    newArray.push(`Thank you, ${cards[i]}, for the wonderful ${events} gift!`);
+  }
+
+  return newArray;
+}
+
+writeCards(cards,"surprise");
+
+function countDown() {
+  let i = 10;
+  while (i >= 0){
+    console.log(i--);
+  }
+}
